c433f1f8c1772483966e21cdc0e4b86f94917015
sentinel-i <admin@sentinelinformatique.fr>
2025-06-27 00:56:08 +0200

    Update page.tsx

--- apps/dashboard/src/app/[locale]/(app)/(sidebar)/settings/notifications/page.tsx
+++ apps/dashboard/src/app/[locale]/(app)/(sidebar)/settings/notifications/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"
+
 import { NotificationsSettingsList } from "@/components/notifications-settings-list";
 import type { Metadata } from "next";
 import { Suspense } from "react";

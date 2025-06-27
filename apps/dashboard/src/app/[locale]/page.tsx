8112b0bb8415a49717fb99043e14d0530945cae1
sentinel-i <admin@sentinelinformatique.fr>
2025-06-27 15:27:40 +0200

    Update page.tsx

--- apps/dashboard/src/app/[locale]/page.tsx
+++ apps/dashboard/src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function RedirectHome() {
-  redirect("/en/inbox"); // ou toute route que tu sais valide
+export default function Page() {
+  redirect("/inbox");
 }

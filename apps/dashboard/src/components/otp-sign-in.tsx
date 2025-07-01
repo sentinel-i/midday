238e59f7c27e5d6836baa787225e9b58339c1665
sentinel-i <admin@sentinelinformatique.fr>
2025-07-01 18:30:38 +0200

    Update otp-sign-in.tsx

--- apps/dashboard/src/components/otp-sign-in.tsx
+++ apps/dashboard/src/components/otp-sign-in.tsx
@@ -42,7 +42,11 @@ export function OTPSignIn({ className }: Props) {
 
     setEmail(email);
 
-    await supabase.auth.signInWithOtp({ email });
+  await supabase.auth.signInWithOtp({
+    email,
+    options: { shouldSendLink: false }
+  });
+
 
     setSent(true);
     setLoading(false);
